2e4b961e54ccfc7752e4942ed720f5d8329d729e
sukumar-kollabathula <sukumar.kollabathula@gmail.com>
2025-09-21 16:00:07 +0530

    mobile responsive

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <div className="border-b-1 flex justify-around h-16 items-center w-full">
             <h1 className="text-lg lg:text-2xl font-bold">Blogverse</h1>
-            <Link to ="/home" className="text-black cursor-pointer hover:bg-gray-600 px-5 py-2 rounded-2xl">Home</Link>
+            <Link to ="/" className="text-black cursor-pointer hover:bg-gray-600 px-5 py-2 rounded-2xl">Home</Link>
 
             <div>
             <Link to ="/signin"className="font-bold text-gray-600 cursor-pointer m-5 ">SignIn</Link>
